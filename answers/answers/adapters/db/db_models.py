feda82e5df673cd441a0310aa338a994c833f14d
Rychanya <rychanya@gmail.com>
2023-02-07 02:10:23 +0300

    🚧

--- answers/answers/adapters/db/db_models.py
+++ answers/answers/adapters/db/db_models.py
@@ -29,23 +29,23 @@ class Question(Base):
     id: Mapped[str] = mapped_column(primary_key=True, default_factory=new_id)
     created_by: Mapped[str] = mapped_column(ForeignKey("user.id"), init=False)
     user: Mapped[User] = relationship(init=False)
-    options: Mapped[list["OptionItem"]] = relationship(init=False)
-    extra_options: Mapped[list["ExtraOptionItem"]] = relationship(init=False)
+    option: Mapped["Option"] = relationship(init=False)
     answers: Mapped[list["Answer"]] = relationship(init=False)
 
 
 class Option(Base):
     __tablename__ = "option"
 
+    question_id: Mapped[str] = mapped_column(ForeignKey("question.id"))
     id: Mapped[str] = mapped_column(primary_key=True, default_factory=new_id)
+    options: Mapped[list["OptionItem"]] = relationship(init=False)
+    extra_options: Mapped[list["ExtraOptionItem"]] = relationship(init=False)
 
 
 class OptionItem(Base):
     __tablename__ = "option_item"
 
-    option_id: Mapped[str] = mapped_column(
-        ForeignKey("option.id"), primary_key=True
-    )
+    option_id: Mapped[str] = mapped_column(ForeignKey("option.id"), primary_key=True)
     text: Mapped[str] = mapped_column(
         primary_key=True,
     )
@@ -54,9 +54,7 @@ class OptionItem(Base):
 class ExtraOptionItem(Base):
     __tablename__ = "extra_option_item"
 
-    option_id: Mapped[str] = mapped_column(
-        ForeignKey("option.id"), primary_key=True
-    )
+    option_id: Mapped[str] = mapped_column(ForeignKey("option.id"), primary_key=True)
     text: Mapped[str] = mapped_column(
         primary_key=True,
     )
--- answers/answers/adapters/db/question.py
+++ answers/answers/adapters/db/question.py
@@ -3,24 +3,37 @@ from sqlalchemy.exc import NoResultFound
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
-from answers.adapters.db.db_models import Option, Question, User
+from answers.adapters.db.db_models import (
+    OptionItem,
+    Question,
+    User,
+    ExtraOptionItem,
+    Option,
+)
 
 
 async def get_question(
-    text: str, question_type: str, session: AsyncSession
+    text: str,
+    question_type: str,
+    option: tuple[str],
+    extra_option: tuple[str],
+    session: AsyncSession,
 ) -> Question | None:
     res = await session.execute(
         select(Question)
         .options(
-            joinedload(Question.options),
-            joinedload(Question.extra_options),
+            joinedload(Question.option),
             joinedload(Question.answers),
             joinedload(Question.user),
         )
         .where(
             Question.text == text,
             Question.question_type == question_type,
-            Question.options.any(Option.text == "444"),
+            *(Option.options.any(OptionItem.text == op) for op in option),
+            *(
+                Option.extra_options.any(ExtraOptionItem.text == eop)
+                for eop in extra_option
+            )
         )
         .limit(1)
     )
@@ -31,10 +44,19 @@ async def get_question(
 
 
 async def get_or_create(
-    text: str, question_type: str, user: User, session: AsyncSession
+    text: str,
+    question_type: str,
+    user: User,
+    option: tuple[str],
+    extra_option: tuple[str],
+    session: AsyncSession,
 ):
     question = await get_question(
-        text=text, question_type=question_type, session=session
+        text=text,
+        question_type=question_type,
+        option=option,
+        extra_option=extra_option,
+        session=session,
     )
     if question:
         return question
