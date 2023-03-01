e3e457bbada1e400e41dc3416543285cfd610f5d
Rychanya <rychanya@gmail.com>
2023-03-01 12:36:29 +0300

    🚧

--- answers/tests/unit/repositary/test_answers.py
+++ answers/tests/unit/repositary/test_answers.py
@@ -1,60 +1,69 @@
 import pytest
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
 from answers.adapters.db.db_models import User
-from answers.adapters.repository.sql_alchemy import SQLAlchemyAnswerRepository
+from answers.adapters.repository import AbstractRepository
 from answers.domain.commands import CreateAnswer
 
 pytestmark = pytest.mark.anyio
 
 
 async def test_empty(
-    sqlite_memory_session_maker: async_sessionmaker[AsyncSession],
     test_answer_dto: CreateAnswer,
+    sql_repository: AbstractRepository,
 ):
-    async with sqlite_memory_session_maker() as session:
-        rep = SQLAlchemyAnswerRepository(session=session)
-        res = await rep.get(dto=test_answer_dto)
-        assert res is None
+    async with sql_repository:
+        res = await sql_repository.answers.get(dto=test_answer_dto)
+        await sql_repository.commit()
+
+    assert res is None
 
 
 async def test_exist(
-    sqlite_memory_session_maker: async_sessionmaker[AsyncSession],
+    sql_repository: AbstractRepository,
     test_answer_dto: CreateAnswer,
     user_in_db: User,
 ):
-    async with sqlite_memory_session_maker() as session:
-        rep = SQLAlchemyAnswerRepository(session=session)
-        answer_in_db = await rep.create(dto=test_answer_dto, user_id=user_in_db.id)
-        res = await rep.get(dto=test_answer_dto)
-        assert res is not None
-        assert res.answer_id == answer_in_db.answer_id
+    async with sql_repository:
+        answer_in_db = await sql_repository.answers.create(
+            dto=test_answer_dto, user_id=user_in_db.id
+        )
+        await sql_repository.commit()
+
+        res = await sql_repository.answers.get(dto=test_answer_dto)
+
+    assert res is not None
+    assert res.answer_id == answer_in_db.answer_id
 
 
 async def test_exist_reverse(
-    sqlite_memory_session_maker: async_sessionmaker[AsyncSession],
+    sql_repository: AbstractRepository,
     test_answer_dto: CreateAnswer,
     user_in_db: User,
 ):
-    async with sqlite_memory_session_maker() as session:
-        rep = SQLAlchemyAnswerRepository(session=session)
-        answer_in_db = await rep.create(dto=test_answer_dto, user_id=user_in_db.id)
+    async with sql_repository:
+        answer_in_db = await sql_repository.answers.create(
+            dto=test_answer_dto, user_id=user_in_db.id
+        )
+        await sql_repository.commit()
+
         test_answer_dto.answer.reverse()
-        res = await rep.get(dto=test_answer_dto)
-        print(res)
-        assert res is not None
-        assert res.answer_id == answer_in_db.answer_id
+        res = await sql_repository.answers.get(dto=test_answer_dto)
+
+    assert res is not None
+    assert res.answer_id == answer_in_db.answer_id
 
 
 async def test_get_or_create_twice(
-    sqlite_memory_session_maker: async_sessionmaker[AsyncSession],
+    sql_repository: AbstractRepository,
     test_answer_dto: CreateAnswer,
     user_in_db: User,
 ):
-    async with sqlite_memory_session_maker() as session:
-        rep = SQLAlchemyAnswerRepository(session=session)
-        a1 = await rep.get_or_create(dto=test_answer_dto, user_id=user_in_db.id)
-        a2 = await rep.get_or_create(
+    async with sql_repository:
+        a1 = await sql_repository.answers.get_or_create(
+            dto=test_answer_dto, user_id=user_in_db.id
+        )
+        await sql_repository.commit()
+        a2 = await sql_repository.answers.get_or_create(
             dto=test_answer_dto,
             user_id=user_in_db.id,
         )
--- answers/tests/unit/repositary/test_questions.py
+++ answers/tests/unit/repositary/test_questions.py
@@ -1,9 +1,8 @@
 import pytest
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
 from answers.adapters.db.db_models import User
-from answers.adapters.repository.sql_alchemy import SQLAlchemyQuestionRepository
+from answers.adapters.repository import AbstractRepository
 from answers.domain.commands import CreateQuestion
 from answers.domain.models import QuestionType
 from answers.domain.specification import TextContains
@@ -12,137 +11,172 @@ pytestmark = pytest.mark.anyio
 
 
 async def test_empty(
-    sqlite_memory_session_maker: async_sessionmaker[AsyncSession],
+    sql_repository: AbstractRepository,
     test_question_dto: CreateQuestion,
 ):
-    async with sqlite_memory_session_maker() as session:
-        rep = SQLAlchemyQuestionRepository(session=session)
-        res = await rep.get(dto=test_question_dto)
-        assert res is None
+    async with sql_repository:
+        res = await sql_repository.questions.get(dto=test_question_dto)
+        await sql_repository.commit()
+
+    assert res is None
 
 
 async def test_exist(
-    sqlite_memory_session_maker: async_sessionmaker[AsyncSession],
+    sql_repository: AbstractRepository,
     test_question_dto: CreateQuestion,
     user_in_db: User,
 ):
-    async with sqlite_memory_session_maker() as session:
-        rep = SQLAlchemyQuestionRepository(session=session)
-        question_dto_in_db = await rep.create(
+    async with sql_repository:
+        question_dto_in_db = await sql_repository.questions.create(
             dto=test_question_dto, user_id=user_in_db.id
         )
-        res = await rep.get(dto=test_question_dto)
-        assert res is not None
-        assert res.id == question_dto_in_db.id
+        await sql_repository.commit()
+        res = await sql_repository.questions.get(dto=test_question_dto)
+
+    assert res is not None
+    assert res.id == question_dto_in_db.id
 
 
 async def test_exist_reverse(
-    sqlite_memory_session_maker: async_sessionmaker[AsyncSession],
+    sql_repository: AbstractRepository,
     test_question_dto: CreateQuestion,
     user_in_db: User,
 ):
-    async with sqlite_memory_session_maker() as session:
-        rep = SQLAlchemyQuestionRepository(session=session)
-        question_dto_in_db = await rep.create(
+    async with sql_repository:
+        question_dto_in_db = await sql_repository.questions.create(
             dto=test_question_dto, user_id=user_in_db.id
         )
+        await sql_repository.commit()
         test_question_dto.options.reverse()
-        res = await rep.get(dto=test_question_dto)
-        assert res is not None
-        assert res.id == question_dto_in_db.id
+        res = await sql_repository.questions.get(dto=test_question_dto)
+
+    assert res is not None
+    assert res.id == question_dto_in_db.id
 
 
 async def test_exist_with_extra(
-    sqlite_memory_session_maker: async_sessionmaker[AsyncSession],
+    sql_repository: AbstractRepository,
     test_question_dto: CreateQuestion,
     user_in_db: User,
 ):
-    async with sqlite_memory_session_maker() as session:
-        rep = SQLAlchemyQuestionRepository(session=session)
-        await rep.create(dto=test_question_dto, user_id=user_in_db.id)
+    async with sql_repository:
+        await sql_repository.questions.create(
+            dto=test_question_dto, user_id=user_in_db.id
+        )
+        await sql_repository.commit()
+
         test_question_dto.options.append("3")
         test_question_dto.extra_options.append("c")
-        res = await rep.get(dto=test_question_dto)
-        assert res is None
+        res = await sql_repository.questions.get(dto=test_question_dto)
+
+    assert res is None
 
 
 async def test_exist_same_size(
-    sqlite_memory_session_maker: async_sessionmaker[AsyncSession],
+    sql_repository: AbstractRepository,
     test_question_dto: CreateQuestion,
     user_in_db: User,
 ):
-    async with sqlite_memory_session_maker() as session:
-        rep = SQLAlchemyQuestionRepository(session=session)
-        await rep.create(dto=test_question_dto, user_id=user_in_db.id)
+    async with sql_repository:
+        await sql_repository.questions.create(
+            dto=test_question_dto, user_id=user_in_db.id
+        )
+        await sql_repository.commit()
+
         test_question_dto.options[0] = "4"
         test_question_dto.extra_options[0] = "c"
-        res = await rep.get(dto=test_question_dto)
-        assert res is None
+        res = await sql_repository.questions.get(dto=test_question_dto)
+
+    assert res is None
 
 
 async def test_exist_different_question(
-    sqlite_memory_session_maker: async_sessionmaker[AsyncSession],
+    sql_repository: AbstractRepository,
     test_question_dto: CreateQuestion,
     user_in_db: User,
 ):
-    async with sqlite_memory_session_maker() as session:
-        rep = SQLAlchemyQuestionRepository(session=session)
-        await rep.create(dto=test_question_dto, user_id=user_in_db.id)
+    async with sql_repository:
+        await sql_repository.questions.create(
+            dto=test_question_dto, user_id=user_in_db.id
+        )
+        await sql_repository.commit()
+
         test_question_dto.question_text = "different_question"
-        res = await rep.get(
+        res = await sql_repository.questions.get(
             dto=test_question_dto,
         )
-        assert res is None
+
+    assert res is None
 
 
 async def test_exist_different_question_type(
-    sqlite_memory_session_maker: async_sessionmaker[AsyncSession],
+    sql_repository: AbstractRepository,
     test_question_dto: CreateQuestion,
     user_in_db: User,
 ):
-    async with sqlite_memory_session_maker() as session:
-        rep = SQLAlchemyQuestionRepository(session=session)
-        await rep.create(dto=test_question_dto, user_id=user_in_db.id)
+    async with sql_repository:
+        await sql_repository.questions.create(
+            dto=test_question_dto, user_id=user_in_db.id
+        )
+        await sql_repository.commit()
+
         test_question_dto.question_type = QuestionType.MANY
-        res = await rep.get(dto=test_question_dto)
-        assert res is None
+        res = await sql_repository.questions.get(dto=test_question_dto)
+
+    assert res is None
 
 
 async def test_get_or_create_twice(
-    sqlite_memory_session_maker: async_sessionmaker[AsyncSession],
+    sql_repository: AbstractRepository,
     test_question_dto: CreateQuestion,
     user_in_db: User,
 ):
-    async with sqlite_memory_session_maker() as session:
-        rep = SQLAlchemyQuestionRepository(session=session)
-        q1 = await rep.get_or_create(dto=test_question_dto, user_id=user_in_db.id)
-        q2 = await rep.get_or_create(dto=test_question_dto, user_id="new user id")
+    async with sql_repository:
+        q1 = await sql_repository.questions.get_or_create(
+            dto=test_question_dto, user_id=user_in_db.id
+        )
+        await sql_repository.commit()
+        q2 = await sql_repository.questions.get_or_create(
+            dto=test_question_dto, user_id="new user id"
+        )
+        await sql_repository.commit()
 
-        assert q1.id == q2.id
-        assert q1.created_by == q2.created_by
+    assert q1.id == q2.id
+    assert q1.created_by == q2.created_by
 
 
 async def test_list(
-    sqlite_memory_session_maker: async_sessionmaker[AsyncSession],
+    sql_repository: AbstractRepository,
     test_question_dto: CreateQuestion,
     user_in_db: User,
 ):
-    async with sqlite_memory_session_maker() as session:
-        rep = SQLAlchemyQuestionRepository(session=session)
-        assert len(await rep.list([])) == 0
-        q1 = await rep.get_or_create(dto=test_question_dto, user_id=user_in_db.id)
-        q_list = await rep.list([TextContains(q=test_question_dto.question_text[:1])])
-        assert len(q_list) == 1
-        assert q_list[0].id == q1.id
+    async with sql_repository:
+        assert len(await sql_repository.questions.list([])) == 0
+        q1 = await sql_repository.questions.get_or_create(
+            dto=test_question_dto, user_id=user_in_db.id
+        )
+        await sql_repository.commit()
+
+        q_list = await sql_repository.questions.list(
+            [TextContains(q=test_question_dto.question_text[:1])]
+        )
+
+    assert len(q_list) == 1
+    assert q_list[0].id == q1.id
 
 
 async def test_unique(
-    sqlite_memory_session_maker: async_sessionmaker[AsyncSession],
+    sql_repository: AbstractRepository,
     test_question_dto: CreateQuestion,
     user_in_db: User,
 ):
-    async with sqlite_memory_session_maker() as session:
-        rep = SQLAlchemyQuestionRepository(session=session)
-        await rep.create(dto=test_question_dto, user_id=user_in_db.id)
-        with pytest.raises(IntegrityError):
-            await rep.create(dto=test_question_dto, user_id=user_in_db.id)
+    async with sql_repository:
+        await sql_repository.questions.create(
+            dto=test_question_dto, user_id=user_in_db.id
+        )
+        await sql_repository.commit()
+
+        with pytest.raises(expected_exception=IntegrityError):
+            await sql_repository.questions.create(
+                dto=test_question_dto, user_id=user_in_db.id
+            )
