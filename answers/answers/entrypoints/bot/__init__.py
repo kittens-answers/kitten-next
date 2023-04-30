d32cbe4f08082a6aff3e64e48e711b51935df89d
Rychanya <rychanya@gmail.com>
2023-04-30 03:25:20 +0300

    🚀

--- answers/answers/entrypoints/bot/__init__.py
+++ answers/answers/entrypoints/bot/__init__.py
@@ -1,30 +1,30 @@
 import logging
 from telegram import MenuButtonWebApp, Update, WebAppInfo
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
-from answers.settings import BaseSettings
+from os import environ
 
-logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
-
-
-class TGSettings(BaseSettings):
-    token: str
-    menu_url: str
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 
 
-tg_settings = TGSettings()  # type: ignore
+token: str = environ.get("kittens_token", "")
+menu_url: str = environ.get("kittens_menu_url", "")
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     assert update.effective_chat
     await context.bot.setChatMenuButton(
         chat_id=update.effective_chat.id,
-        menu_button=MenuButtonWebApp(text="menu", web_app=WebAppInfo(url=tg_settings.menu_url)),
+        menu_button=MenuButtonWebApp(text="menu", web_app=WebAppInfo(url=menu_url)),
     )
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
+    )
 
 
 if __name__ == "__main__":
-    application = ApplicationBuilder().token(tg_settings.token).build()
+    application = ApplicationBuilder().token(token).build()
 
     start_handler = CommandHandler("start", start)
     application.add_handler(start_handler)
